beb11476258b8f57b14e1cff0f67a55cbc260258
shenaoro <shenaorj@gmail.com>
2023-10-10 21:38:56 -0500

    mobile terminada

--- public_html/main.js
+++ public_html/main.js
@@ -10,10 +10,28 @@ const videoVidaUtil = document.getElementById('videoVidaUtil');
 const toggleSeguridadPacientes = document.getElementById('toggleSeguridadPacientes');
 const videoSeguridadPacientes = document.getElementById('videoSeguridadPacientes');
 
+const bottomLimpieza = document.getElementById('bottomLimpieza');
+const ConseVideo1 = document.getElementById('ConseVideo1');
+
+const bottomMantenimiento = document.getElementById('bottomMantenimiento');
+const ConseVideo2 = document.getElementById('ConseVideo2');
+
+const bottomAlmacenamiento = document.getElementById('bottomAlmacenamiento');
+const ConseVideo3 = document.getElementById('ConseVideo3');
+
+const bottomCapacitacion = document.getElementById('bottomCapacitacion');
+const ConseVideo4 = document.getElementById('ConseVideo4');
+
+
+
 menuDisplay.addEventListener('click', toggleMobileMenu);
 toggleFuncionamiento.addEventListener('click', toggleVideoFuncionamiento);
 toggleVidaUtil.addEventListener('click', toggleVideoVidaUtil);
 toggleSeguridadPacientes.addEventListener('click', togglevideoSeguridadPacientes);
+bottomLimpieza.addEventListener('click', consejoVideo1);
+bottomMantenimiento.addEventListener('click', consejoVideo2);
+bottomAlmacenamiento.addEventListener('click', consejoVideo3);
+bottomCapacitacion.addEventListener('click', consejoVideo4);
 
 function toggleMobileMenu () {
     mobileMenu.classList.toggle('hidden');
@@ -30,4 +48,32 @@ function togglevideoSeguridadPacientes () {
 
 function closeMobileMenu() {
     mobileMenu.classList.add("hidden");
-  }
+}
+
+function consejoVideo1() {
+    ConseVideo1.classList.toggle("hidden");
+    ConseVideo2.classList.add("hidden");
+    ConseVideo3.classList.add("hidden");
+    ConseVideo4.classList.add("hidden");
+}
+
+function consejoVideo2() {
+    ConseVideo2.classList.toggle("hidden");
+    ConseVideo1.classList.add("hidden");
+    ConseVideo3.classList.add("hidden");
+    ConseVideo4.classList.add("hidden");
+}
+
+function consejoVideo3() {
+    ConseVideo3.classList.toggle("hidden");
+    ConseVideo2.classList.add("hidden");
+    ConseVideo1.classList.add("hidden");
+    ConseVideo4.classList.add("hidden");
+}
+
+function consejoVideo4() {
+    ConseVideo4.classList.toggle("hidden");
+    ConseVideo3.classList.add("hidden");
+    ConseVideo2.classList.add("hidden");
+    ConseVideo1.classList.add("hidden");
+}
